c3c84d807063e1b8daa2f1a54c061fcb77226e7e
tz-gif <davimrodrigues3@gmail.com>
2026-09-18 10:22:49 -0300

    Primeira aula

---
+++ AULA02/conceito.js
@@ -0,0 +1,45 @@
+// INICIO DO BACKEND---JAVASCRIPT
+
+// VARIAVEIS
+
+// VAR É MUITO ANTIGO E PODE SER DECLARADO E MUDADO O VALOR
+var idade = 10
+var idade =20// NÃO GERA ERRO
+
+// MAIS MODERNO, PODE MUDAR DE VALOR, MAS NÃO PODE SER REDECLARADO
+let nome ="julia"
+nome = "Dafny" ;//PERMITIDO
+
+// LET nome de novo não funciona porque ja existe uma variavel LET
+
+//  const é fixo, não pode mudar o valor/ NEM DECLARA
+const pi = 3.14;
+//  pi = 40; // ERRO
+
+//----------------------------- 
+// FORMAS DE ESCREVER CODIGOS
+// ---------------------------
+
+
+// CAMEL CASE ------* A mais Famosa
+//  -Primeira palavra minuscula 
+//  -Palavras seguintes começam com maiusculo
+
+// let nomecompleto, let idadedoUsuario; functioncalcularidade(){}
+
+// ----------------------------------
+// PASCALCASE
+// - Todas as palavras começam com letra maiuscula
+
+// class UsuarioSistemas {
+//    constructor(IDADE){
+//       this.nome = nome;
+//       this.idade = idade;
+//    }
+// };
+
+// ----------------------------------
+// SNAKE CASE
+// - Palavras separadas por underscore _
+
+// let nome_completo; let total_vendas;
---
+++ AULA02/index.js
@@ -0,0 +1,11 @@
+var cor = "azul"
+var cor = "varmelho"
+console.log(cor);
+
+let fruta = "manga";
+fruta = "uva";
+console.log(fruta);// NESSA CASO FUNCIONA RENOMEAR
+
+const pi = 3.14;
+// pi = 3.15; VAI DAR ERRO 
+console.log(pi);
